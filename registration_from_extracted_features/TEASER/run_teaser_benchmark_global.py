9f02fe3181b7f66afb9fab7f5c4b2b4eafddceff
Federica Di Lauro <federicadilauro1998@gmail.com>
2022-04-08 12:01:21 +0000

    run tests on all problems

--- registration_from_extracted_features/TEASER/run_teaser_benchmark_global.py
+++ registration_from_extracted_features/TEASER/run_teaser_benchmark_global.py
@@ -32,65 +32,53 @@ base_command = ( f'{PY3}' + ' teaser_benchmark.py'
                 f' --inlier_selection_mode={INLIER_SELECTION_MODE}' +
                 f' --kcore_heuristic_threshold={KCORE_HEURISTIC_THRESHOLD}')
 
-problem_txts = ['tum/long_office_household_global.txt',
+problem_txts = ['kaist/urban05_global.txt',
+                'eth/apartment_global.txt', 
+                'eth/gazebo_summer_global.txt',
+                'eth/gazebo_winter_global.txt',
+                'eth/hauptgebaude_global.txt',
+                'eth/plain_global.txt',
+                'eth/stairs_global.txt',
+                'eth/wood_autumn_global.txt',
+                'eth/wood_summer_global.txt',
+                'tum/long_office_household_global.txt',
                 'tum/pioneer_slam_global.txt',
-                'planetary/box_met_global.txt']
+                'tum/pioneer_slam3_global.txt',
+                'planetary/box_met_global.txt',
+                'planetary/p2at_met_global.txt',
+                'planetary/planetary_map_global.txt']
 
-pcd_dirs = ['tum/long_office_household/',
+pcd_dirs = ['kaist/urban05/',
+            'eth/apartment/', 
+            'eth/gazebo_summer/',
+            'eth/gazebo_winter/',
+            'eth/hauptgebaude/',
+            'eth/plain/',
+            'eth/stairs/',
+            'eth/wood_autumn/',
+            'eth/wood_summer/',
+            'tum/long_office_household/',
             'tum/pioneer_slam/',
-            'planetary/box_met/']
+            'tum/pioneer_slam3/',
+            'planetary/box_met/',
+            'planetary/p2at_met/',
+            'planetary/p2at_met/']
 
-features_dirs = ['tum/long_office_household/',
+features_dirs = ['kaist/urban05/',
+                'eth/apartment/', 
+                'eth/gazebo_summer/',
+                'eth/gazebo_winter/',
+                'eth/hauptgebaude/',
+                'eth/plain/',
+                'eth/stairs/',
+                'eth/wood_autumn/',
+                'eth/wood_summer/',
+                'tum/long_office_household/',
                 'tum/pioneer_slam/',
-                'planetary/box_met/']
-
-# problem_txts = ['kaist/urban05_global.txt',
-#                 'eth/apartment_global.txt', 
-#                 'eth/gazebo_summer_global.txt',
-#                 'eth/gazebo_winter_global.txt',
-#                 'eth/hauptgebaude_global.txt',
-#                 'eth/plain_global.txt',
-#                 'eth/stairs_global.txt',
-#                 'eth/wood_autumn_global.txt',
-#                 'eth/wood_summer_global.txt',
-#                 'tum/long_office_household_global.txt',
-#                 'tum/pioneer_slam_global.txt',
-#                 'tum/pioneer_slam3_global.txt',
-#                 'planetary/box_met_global.txt',
-#                 'planetary/p2at_met_global.txt',
-#                 'planetary/planetary_map_global.txt']
-
-# pcd_dirs = ['kaist/urban05/',
-#             'eth/apartment/', 
-#             'eth/gazebo_summer/',
-#             'eth/gazebo_winter/',
-#             'eth/hauptgebaude/',
-#             'eth/plain/',
-#             'eth/stairs/',
-#             'eth/wood_autumn/',
-#             'eth/wood_summer/',
-#             'tum/long_office_household/',
-#             'tum/pioneer_slam/',
-#             'tum/pioneer_slam3/',
-#             'planetary/box_met/',
-#             'planetary/p2at_met/',
-#             'planetary/p2at_met/']
-
-# features_dirs = ['kaist/urban05/',
-#                 'eth/apartment/', 
-#                 'eth/gazebo_summer/',
-#                 'eth/gazebo_winter/',
-#                 'eth/hauptgebaude/',
-#                 'eth/plain/',
-#                 'eth/stairs/',
-#                 'eth/wood_autumn/',
-#                 'eth/wood_summer/',
-#                 'tum/long_office_household/',
-#                 'tum/pioneer_slam/',
-#                 'tum/pioneer_slam3/',
-#                 'planetary/box_met/',
-#                 'planetary/p2at_met/',
-#                 'planetary/planetary_map/']
+                'tum/pioneer_slam3/',
+                'planetary/box_met/',
+                'planetary/p2at_met/',
+                'planetary/planetary_map/']
 
 commands = []
 
@@ -102,5 +90,5 @@ for problem_txt, pcd_dir, features_dir in zip(problem_txts, pcd_dirs, features_d
                     f' --output_dir={RESULTS_DIR}')
     commands.append(full_command)
 
-pool = Pool(1)
+pool = Pool(4)
 pool.map(os.system, commands)
