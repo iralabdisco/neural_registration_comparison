e49cbd3b29b5f20873ac9a8788a8524bafed441b
Federica Di Lauro <federicadilauro1998@gmail.com>
2022-04-21 09:47:50 +0000

    WIP ctrl c to stop pool

--- registration_from_extracted_features/RANSAC/run_ransac_benchmark_global.py
+++ registration_from_extracted_features/RANSAC/run_ransac_benchmark_global.py
@@ -1,62 +1,48 @@
 import glob, sys, os
+import signal
 from multiprocessing import Pool
 
-PY3="python3"
-BENCHMARK_DIR="/neural_comparison/point_clouds_registration_benchmark/"
-FEATURES_DIR="/neural_comparison/experiments/3DFeatNet/feature_extraction_voxelgrid_0.1/"
-RESULTS_DIR="/root/neural_registration_comparison/results/3DFeatNet/results_ransac_voxelgrid_0.1/"
+def main():
+    PY3="python3"
+    BENCHMARK_DIR="/neural_comparison/point_clouds_registration_benchmark/"
+    FEATURES_DIR="/neural_comparison/experiments/FCGF/features/"
+    RESULTS_DIR="/root/neural_registration_comparison/results/FCGF"
 
-DISTANCE_METRIC="euclidean" #for valid metrics see https://github.com/scikit-learn/scikit-learn/issues/4520#issuecomment-89837739
+    DISTANCE_METRIC="euclidean" #for valid metrics see https://github.com/scikit-learn/scikit-learn/issues/4520#issuecomment-89837739
 
-VOXEL_SIZE = 0.1
+    VOXEL_SIZE = 0.2
 
-MAX_CORRESPONDENCE_DISTANCE = VOXEL_SIZE * 1.5
-ESTIMATION_METHOD = "TransformationEstimationPointToPoint"
-RANSAC_N = 3
-RANSAC_CONFIDENCE = 0.999
-RANSAC_MAX_ITERATION = 100000
+    MAX_CORRESPONDENCE_DISTANCE = VOXEL_SIZE * 1.5
+    ESTIMATION_METHOD = "TransformationEstimationPointToPoint"
+    RANSAC_N = 3
+    RANSAC_CONFIDENCE = 0.999
+    RANSAC_MAX_ITERATION = 100000
 
-ransac_command = ( f'{PY3}' + ' ransac_benchmark.py'
-                f' --distance_metric={DISTANCE_METRIC}' +
-                f' --max_correspondence_distance={MAX_CORRESPONDENCE_DISTANCE}' +
-                f' --estimation_method={ESTIMATION_METHOD}' +
-                f' --ransac_n={RANSAC_N}' +
-                f' --ransac_confidence={RANSAC_CONFIDENCE}' +
-                f' --ransac_max_iteration={RANSAC_MAX_ITERATION}')
+    ransac_command = ( f'{PY3}' + ' ransac_benchmark.py'
+                    f' --distance_metric={DISTANCE_METRIC}' +
+                    f' --max_correspondence_distance={MAX_CORRESPONDENCE_DISTANCE}' +
+                    f' --estimation_method={ESTIMATION_METHOD}' +
+                    f' --ransac_n={RANSAC_N}' +
+                    f' --ransac_confidence={RANSAC_CONFIDENCE}' +
+                    f' --ransac_max_iteration={RANSAC_MAX_ITERATION}')
 
-problem_txts = ['kaist/urban05_global.txt',
-                'eth/apartment_global.txt', 
-                'eth/gazebo_summer_global.txt',
-                'eth/gazebo_winter_global.txt',
-                'eth/hauptgebaude_global.txt',
-                'eth/plain_global.txt',
-                'eth/stairs_global.txt',
-                'eth/wood_autumn_global.txt',
-                'eth/wood_summer_global.txt',
-                'tum/long_office_household_global.txt',
-                'tum/pioneer_slam_global.txt',
-                'tum/pioneer_slam3_global.txt',
-                'planetary/box_met_global.txt',
-                'planetary/p2at_met_global.txt',
-                'planetary/planetary_map_global.txt']
+    problem_txts = ['kaist/urban05_global.txt',
+                    'eth/apartment_global.txt', 
+                    'eth/gazebo_summer_global.txt',
+                    'eth/gazebo_winter_global.txt',
+                    'eth/hauptgebaude_global.txt',
+                    'eth/plain_global.txt',
+                    'eth/stairs_global.txt',
+                    'eth/wood_autumn_global.txt',
+                    'eth/wood_summer_global.txt',
+                    'tum/long_office_household_global.txt',
+                    'tum/pioneer_slam_global.txt',
+                    'tum/pioneer_slam3_global.txt',
+                    'planetary/box_met_global.txt',
+                    'planetary/p2at_met_global.txt',
+                    'planetary/planetary_map_global.txt']
 
-pcd_dirs = ['kaist/urban05/',
-            'eth/apartment/', 
-            'eth/gazebo_summer/',
-            'eth/gazebo_winter/',
-            'eth/hauptgebaude/',
-            'eth/plain/',
-            'eth/stairs/',
-            'eth/wood_autumn/',
-            'eth/wood_summer/',
-            'tum/long_office_household/',
-            'tum/pioneer_slam/',
-            'tum/pioneer_slam3/',
-            'planetary/box_met/',
-            'planetary/p2at_met/',
-            'planetary/p2at_met/']
-
-features_dirs = ['kaist/urban05/',
+    pcd_dirs = ['kaist/urban05/',
                 'eth/apartment/', 
                 'eth/gazebo_summer/',
                 'eth/gazebo_winter/',
@@ -70,17 +56,42 @@ features_dirs = ['kaist/urban05/',
                 'tum/pioneer_slam3/',
                 'planetary/box_met/',
                 'planetary/p2at_met/',
-                'planetary/planetary_map/']
+                'planetary/p2at_met/']
 
-commands = []
+    features_dirs = ['kaist/urban05/',
+                    'eth/apartment/', 
+                    'eth/gazebo_summer/',
+                    'eth/gazebo_winter/',
+                    'eth/hauptgebaude/',
+                    'eth/plain/',
+                    'eth/stairs/',
+                    'eth/wood_autumn/',
+                    'eth/wood_summer/',
+                    'tum/long_office_household/',
+                    'tum/pioneer_slam/',
+                    'tum/pioneer_slam3/',
+                    'planetary/box_met/',
+                    'planetary/p2at_met/',
+                    'planetary/planetary_map/']
 
-for problem_txt, pcd_dir, features_dir in zip(problem_txts, pcd_dirs, features_dirs):
-    full_command = (ransac_command + 
-                    f' --input_txt={BENCHMARK_DIR}/{problem_txt}' +
-                    f' --input_pcd_dir={BENCHMARK_DIR}/{pcd_dir}' +
-                    f' --input_features_dir={FEATURES_DIR}/{features_dir}' +
-                    f' --output_dir={RESULTS_DIR}/')
-    commands.append(full_command)
+    commands = []
 
-pool = Pool(4)
-pool.map(os.system, commands)
+    for problem_txt, pcd_dir, features_dir in zip(problem_txts, pcd_dirs, features_dirs):
+        full_command = (ransac_command + 
+                        f' --input_txt={BENCHMARK_DIR}/{problem_txt}' +
+                        f' --input_pcd_dir={BENCHMARK_DIR}/{pcd_dir}' +
+                        f' --input_features_dir={FEATURES_DIR}/{features_dir}' +
+                        f' --output_dir={RESULTS_DIR}/')
+        commands.append(full_command)
+    
+    pool = Pool(1)
+    try:
+        pool.map_async(os.system, commands)
+        pool.close()
+        pool.join()
+    except KeyboardInterrupt:
+        pool.terminate()
+        pool.join()
+
+if __name__ == '__main__':
+    main()
